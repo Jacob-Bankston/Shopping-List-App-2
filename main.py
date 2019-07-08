97c9c3eab9a0ab19450c4cba4c656d4271bb26a7
Jacob Bankston <jacob.bankston.smile@gmail.com>
2019-07-07 20:43:54 -0500

    First Commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import json
+
+# create a shopping list array to store the store and grocery item information
+# create a store class to store the store information
+# create a grocery item class to store the grocery item information
+
+# SHOPPING LIST ARRAY
+# Let the user ADD, DELETE or VIEW the shopping lists they've created based off of the stores
+# The store should be listed with a name and address
+
+# STORE
+# Let the user ADD, DELETE, or VIEW the total list of items on the shopping list with the quantities listed as well
+# The grocery item should be listed with a name and a quantity
+
+# JSON FILE
+# The user should be able to save the data to a json file upon closing the application
+# The user should be able to load the data from the json file upon opening the application
